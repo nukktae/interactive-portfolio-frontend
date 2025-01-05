87a05b6b22e7b7ea70e23bedfaf997cb2c20cf28
nukktae <anu.bn@yahoo.com>
2025-01-05 00:13:48 -0800

    new changes and updates

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'www.anubilegdemberel.com', 'anubilegdemberel.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.anubilegdemberel.com',
+      },
+    ],
   },
   experimental: {
   },
